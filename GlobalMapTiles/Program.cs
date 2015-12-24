793e47dc22f1f887a7b4dd422c5bfd9d6962fdc2
jmorrison <jmorrison150@yahoo.com>
2015-12-23 19:26:46 -0600

    initial

---
+++ GlobalMapTiles/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Drawing;
+
+namespace GlobalMapTiles {
+	class Program {
+		static void Main(string[] args) {
+			GlobalMapTiles.DrawMap drawMapInstance = new DrawMap();
+			drawMapInstance.initialize();
+
+		}
+	}
+
+
+
+	class DrawMap {
+
+		public void initialize() {
+
+			DateTime startTime = DateTime.Now;
+			string now = startTime.ToString("yyyy.MM.dd HH.mm");
+			Console.WriteLine("Started at " + now);
+
+			var client = new MongoClient("mongodb://localhost:27017");
+			var database = client.GetDatabase("test");
+			var collection = database.GetCollection<BsonDocument>("prop");
+
+			//world
+			process("0", collection);
+			process("1", collection);
+			process("2", collection);
+			process("3", collection);
+
+
+
+			// ////dfw
+			// GlobalMercator pr = new GlobalMercator();
+			// string fortWorth = pr.tileXYToQuadKey(117, 206, 9);
+			// string dallas = pr.tileXYToQuadKey(118, 206, 9);
+			// Task tsk = queryToBitmap(fortWorth, collection);
+			// tsk.Wait();
+			// tsk = queryToBitmap(dallas, collection);
+			// tsk.Wait();
+
+
+
+
+			DateTime endTime = DateTime.Now;
+			TimeSpan elapsedTime = endTime-startTime;
+			Console.WriteLine("elapsedTime = "+elapsedTime);
+			pause();
+
+
+		}
+		void process(string quadTree, IMongoCollection<BsonDocument> collection) {
+
+			Task<long> tsk = queryToBitmap(quadTree, collection);
+			tsk.Wait();
+
+			if(tsk.Result>0) {
+				int zoom = quadTree.Length;
+				int maxZoom = 11;
+				if(zoom <= maxZoom) {
+					string[] childrenTiles = getChildrenTiles(quadTree);
+					foreach(string child in childrenTiles) {
+						process(child, collection);
+					}
+				}
+			}
+
+		}
+		static async Task<long> queryToBitmap(string quadTree, IMongoCollection<BsonDocument> collection) {
+			GlobalMercator proj = new GlobalMercator();
+			int[] tileXYZ = proj.quadKeyToTileXY(quadTree);
+			System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(256, 256);
+			//long count = 0;
+			int zoom = quadTree.Length;
+
+			double[] bounds = proj.tileLatLngBounds(tileXYZ[0], tileXYZ[1], tileXYZ[2]);
+			var builder = Builders<BsonDocument>.Filter;
+			var filter = builder.Exists("nearMin",true) & builder.Gt("lat", bounds[0]) & builder.Lt("lat", bounds[1]) & builder.Gt("lng", bounds[2]) & builder.Lt("lng", bounds[3]);
+			long count = collection.Find(filter).CountAsync().Result;
+			Console.Write(count.ToString()+",");
+			if(count==0) { return count; }
+
+			using(var cursor = await collection.FindAsync(filter)) {
+				while(await cursor.MoveNextAsync()) {
+					var batch = cursor.Current;
+					int current = 0;
+					
+					foreach(var doc in batch) {
+						current++;
+						//if(!(5000<current && current<10000 && current%3==0)){continue;}
+						
+						if(doc["price"].BsonType!=BsonType.Int32) { continue; }
+						
+						int p = (int) doc["price"].AsInt32;
+						double latitude =(double) doc["lat"].AsDouble;
+						double longitude =(double) doc["lng"].AsDouble;  
+						
+						//Draw.price(p , latitude, longitude, zoom, bitmap);
+						Draw.near(doc["price"].AsInt32,doc["nearMin"].AsInt32,doc["nearMax"].AsInt32, doc["lat"].AsDouble, doc["lng"].AsDouble, zoom, bitmap);
+						
+						
+						
+					}
+				}
+			}
+
+			//	for(int i = 0; i < 256; i++) {
+			//			double[] boundsIJ = new double[4];
+			//			boundsIJ[0] = Draw.map(i, 0, 256, bounds[0], bounds[1]);
+			//			boundsIJ[1] = Draw.map(i+1, 0, 256, bounds[0], bounds[1]);
+			//		for(int j = 0; j < 256; j++) {
+			//			boundsIJ[2] = Draw.map(j, 0, 256, bounds[2], bounds[3]);
+			//			boundsIJ[3] = Draw.map(j+1, 0, 256, bounds[2], bounds[3]);
+			//			var filterIJ = builder.Gt("lat", boundsIJ[0]) & builder.Lt("lat", boundsIJ[1]) & builder.Gt("lng", boundsIJ[2]) & builder.Lt("lng", boundsIJ[3]);
+
+			//			FindOptions options = new FindOptions();
+			//			options.BatchSize = 1;
+			//			var documents = await collection.Find(filter).ToListAsync();
+			//			foreach(BsonDocument doc in documents) {
+
+
+			//				//BsonDocument doc = collection.Find(filterIJ).Limit(1)
+			//				//Console.WriteLine(doc["price"].BsonType.ToString());
+			//				//Console.WriteLine(doc["price"].ToString());
+			//				if(doc["price"].BsonType!=BsonType.Int32) { continue; }
+			//				//Console.WriteLine(doc["price"].AsDouble.ToString());
+			//				//Console.WriteLine(((double)doc["price"]).ToString());
+			//				Draw.circle((double) doc["price"].AsInt32, (double) doc["lat"].AsDouble, (double) doc["lng"].AsDouble, zoom, bitmap);
+			//				//count++;
+
+
+
+			//	//draw each property
+
+
+
+			//	var list = await collection.Find(filter).Limit(3000000).ToListAsync();	
+			//foreach(var doc in list){
+			//	if(doc["price"].BsonType!=BsonType.Int32) { continue; }
+			//	Draw.price(doc["price"].AsInt32, doc["lat"].AsDouble, doc["lng"].AsDouble, zoom, bitmap);
+
+
+			////save bitmap
+			string pathString = "./data/"+tileXYZ[2]+"/"+tileXYZ[0]+"/";
+			System.IO.Directory.CreateDirectory(pathString);
+			bitmap.Save("./data/"+tileXYZ[2]+"/"+tileXYZ[0]+"/"+tileXYZ[1]+".png");
+
+
+			return count;
+
+		}
+		string[] getChildrenTiles(string quadTree) {
+			string[] children = new string[4];
+			children[0] = quadTree+"0";
+			children[1] = quadTree+"1";
+			children[2] = quadTree+"2";
+			children[3] = quadTree+"3";
+
+			return children;
+		}
+		private void pause() {
+			Console.WriteLine("Press Enter to Continue...");
+			Console.ReadKey(false);
+		}
+
+	}
+
+
+
+	public class Draw {
+
+		public static int map(int value1, int min1, int max1, int min2, int max2) {
+			int value2 = min2 + ( value1 - min1 ) * ( max2 - min2 ) / ( max1 - min1 );
+			return value2;
+		}
+
+		public static double map(double value1, double min1, double max1, double min2, double max2) {
+			double value2 = min2 + ( value1 - min1 ) * ( max2 - min2 ) / ( max1 - min1 );
+			return value2;
+		}
+		public static void price(int price, double lat, double lng, int zoom, Bitmap bitmap) {
+			int minPrice = 40000;
+			int maxPrice = 800000;
+
+			near(price,minPrice,maxPrice,lat,lng,zoom,bitmap);
+		}
+		
+		public static void near(int price, int nearMin, int nearMax, double lat, double lng, int zoom, Bitmap bitmap) {
+
+			int minPrice = nearMin;
+			int maxPrice = nearMax;
+
+			int r = (int) map(price, minPrice, maxPrice, 0, 255);
+			int g = 255-Math.Abs((int) map(price, minPrice, maxPrice, -255, 255));
+			int b = (int) map(price, minPrice, maxPrice, 255, 0);
+			r = Math.Max(Math.Min(r, 255), 0);
+			g = Math.Max(Math.Min(g, 255), 0);
+			b = Math.Max(Math.Min(b, 255), 0);
+			Color color = Color.FromArgb(255, r, g, b);
+
+			GlobalMercator proj = new GlobalMercator();
+			int[] pixel = proj.latLngToTile(lat, lng, zoom);
+
+			if(zoom<=8) {
+				bitmap.SetPixel(pixel[0], pixel[1], color);
+			} else {
+
+				double size0 = zoom-6.0;
+				int size = (int) Math.Max(size0, 1);
+				int px = ( pixel[0]-(int) ( size*0.5 ) );
+				int py = ( pixel[1]-(int) ( size*0.5 ) );
+				Graphics grf = Graphics.FromImage(bitmap);
+				Brush brsh = new SolidBrush(color);
+				grf.FillEllipse(brsh, px, py, size, size);
+				grf.Dispose();
+			}
+		}
+	}
+}
---
+++ HousePricesDownload/Program.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace HousePricesDownload {
+    class Program {
+        static void Main(string[] args) {
+            HousePricesDownload.MyClass myClassInstance = new MyClass();
+            myClassInstance.initialize();
+        }
+    }
+
+    class MyClass {
+        protected static IMongoClient client;
+        protected static IMongoDatabase test;
+
+
+
+        public void initialize() {
+            //record time
+            DateTime startTime = DateTime.Now;
+            string now = startTime.ToString("yyyy.MM.dd HH.mm");
+            Console.WriteLine("Started at " + now);
+
+
+
+            client = new MongoClient();
+            test = client.GetDatabase("test");
+            IMongoCollection<BsonDocument> collection = test.GetCollection<BsonDocument>("prop1");
+
+            double latMin = -90.0;
+            double latMax = 90.0;
+            double lngMin = -180.0;
+            double lngMax = 180.0;
+
+            int sleep = 2000;
+
+            //dallas
+            latMin =  32.472300;
+            lngMin = -97.610412;
+            latMax = 33.125659;
+            lngMax =  -96.410155;
+
+
+            //double latMin = -73.984750;
+            //double lngMin = 40.777982;
+            double size = 0.01;
+
+
+
+            for(double currentLng = lngMax; currentLng >=lngMin; currentLng -= size) {
+                for(double currentLat = latMax; currentLat >=latMin; currentLat -= size) {
+                    //for(double currentLng = lngMin; currentLng <=lngMax; currentLng += size) {
+                    //    for(double currentLat = latMin; currentLat <=latMax; currentLat += size) {
+                    
+                    Console.WriteLine("("+currentLng+","+currentLat+")");
+                    run(currentLat, currentLng, size, collection);
+                    System.Threading.Thread.Sleep(sleep);
+                }
+            }
+            DateTime endTime = DateTime.Now;
+            TimeSpan elapsedTime = endTime-startTime;
+            Console.WriteLine("elapsedTime = "+elapsedTime);
+            
+            pause();
+        }
+        void run(double latMin, double lngMin, double size, IMongoCollection<BsonDocument> collection) {
+
+            string json = "";
+            int page = 1;
+            //int numPages = 0;
+            dynamic data;
+
+
+            //while((page-1)<=numPages) {
+            try {
+                //json = System.IO.File.ReadAllText(@"C:\Data\Dropbox\john\code\HousePricesDownload\HousePricesDownload\obj\nearbyProperties.json");
+                json = getJSON(latMin, lngMin, size, page);
+
+                data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                Console.WriteLine(data.map.nearbyProperties.Count);
+                Console.Write(data.list.page);
+                Console.Write(" of ");
+                Console.WriteLine(data.list.numPages);
+
+                //insertDB(data, collection);
+                insertRawJSON(json, collection);
+
+                //page = data.list.page +1;
+                //numPages = data.list.numPages;
+                //}
+            } catch {
+                Console.Write("0"); return;
+            }
+        }
+
+        void insertRawJSON(string json, IMongoCollection<BsonDocument> collection) {
+            MongoDB.Bson.BsonDocument document = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json);
+//            BsonDocument document = json.ToBsonDocument();
+            collection.InsertOneAsync(document);
+           
+        }
+        private string getJSON(double lat, double lng, double size, int page) {
+
+            string latMin = string.Format("{0:0}", ( lat )*1000000.0);
+            string lngMin = string.Format("{0:0}", ( lng )*1000000.0);
+            string lngMax = string.Format("{0:0}", ( ( lng+( size ) )*1000000 ));
+            string latMax = string.Format("{0:0}", ( ( lat+( size ) )*1000000 ));
+
+            //string jmorrisonco = "http://www.jmorrison.co";
+            string url = "http://www.zillow.com/search/GetResults.htm"
+                + "?" + "spt=homes"
+                + "&" + "status=110011"
+                + "&" + "lt=111101"
+                + "&" + "ht=111111"
+                + "&" + "pr=,"
+                + "&" + "mp=,"
+                + "&" + "bd=0%2C"
+                + "&" + "ba=0%2C"
+                + "&" + "sf=,"
+                + "&" + "lot=,"
+                + "&" + "yr=,"
+                + "&" + "pho=0"
+                + "&" + "pets=0"
+                + "&" + "parking=0"
+                + "&" + "laundry=0"
+                + "&" + "pnd=0"
+                + "&" + "red=0"
+                + "&" + "zso=0"
+                + "&" + "days=any"
+                + "&" + "ds=all"
+                + "&" + "pmf=1"
+                + "&" + "pf=1"
+                + "&" + "zoom=19"
+                + "&" + "rect="
+                + lngMin
+                + "," +latMin
+                + "," + lngMax
+                + "," + latMax
+                + "&" + "p="
+                + page.ToString()
+                + "&" + "sort=days"
+                + "&" + "search=maplist"
+                + "&" + "disp=1"
+                + "&" + "listright=true"
+                + "&" + "isMapSearch=true"
+                + "&" + "zoom=19";
+
+
+
+
+            HttpWebRequest webReq = (HttpWebRequest) WebRequest.Create(url);
+            webReq.Method = "GET";
+            HttpWebResponse webRes = null;
+            try {
+                webRes  = (HttpWebResponse) webReq.GetResponse();
+            } catch {
+                Console.WriteLine("error = GetResponse");
+            }
+            //feedback
+            //Console.WriteLine(webRes.StatusCode);
+            //Console.WriteLine(webRes.Server);
+
+            Stream answer = webRes.GetResponseStream();
+            StreamReader _answer = new StreamReader(answer);
+            string json = _answer.ReadToEnd();
+
+            //Console.WriteLine(json);
+            return json;
+
+        }
+        private void insertDB(dynamic datas, IMongoCollection<BsonDocument> collection) {
+
+            Console.WriteLine(datas.map.nearbyProperties.Count);
+            for(int i = 0; i < datas.map.nearbyProperties.Count; i++) {
+                //    //add document to mongodb
+
+                if(!( datas.map.nearbyProperties[i][7][0]>0 )) {
+                    Console.Write(i.ToString()+",");
+                    continue;
+                }
+
+                bool isVacantLand = true;
+                int id = 0, type = 0;
+                double lat = 0, lng = 0, price = 0, bed = 0, bath = 0, sf = 0;
+                string img = "";
+
+                try {
+                    id = (int) datas.map.nearbyProperties[i][0];
+                    lat = (double) datas.map.nearbyProperties[i][1]/1000000.0;
+                    lng = (double) datas.map.nearbyProperties[i][2]/1000000.0;
+                    price = (double) datas.map.nearbyProperties[i][7][0];
+
+                    try {
+                        type = (int) datas.map.nearbyProperties[i][4];
+                    } catch {
+                        //Console.WriteLine("error type");
+                    }
+                    try {
+                        bed = (double) datas.map.nearbyProperties[i][7][1];
+                    } catch {
+                    }
+                    try {
+                        bath = (double) datas.map.nearbyProperties[i][7][2];
+                    } catch {
+                    }
+                    try {
+                        sf = (double) datas.map.nearbyProperties[i][7][3];
+                    } catch {
+                    }
+                    try {
+                        isVacantLand = datas.map.nearbyProperties[i][7][4];
+                    } catch {
+                    }
+                    try {
+                        if(datas.map.nearbyProperties[i][7][5]!= null) {
+                            img = (string) datas.map.nearbyProperties[i][7][5];
+                        }
+                    } catch {
+
+
+                        //Console.WriteLine("id="+id);
+                        //Console.WriteLine("price="+price);
+                        //Console.WriteLine("type="+type);
+                        //Console.WriteLine("bed="+bed);}
+                        //Console.WriteLine("bath="+bath);}
+                        //Console.WriteLine("sf="+sf);}
+                        //Console.WriteLine("isVacant="+isVacantLand);
+                        //Console.WriteLine("img="+img);
+                        //Console.WriteLine("---");
+                    }
+
+                } catch {
+                    //Console.Write("error on "+i.ToString());
+                }
+
+
+                if(!( price>0 )) {
+                    Console.WriteLine("price==0");
+                    continue;
+                }
+
+                BsonDocument document;
+                document = new BsonDocument {
+                    
+                    {"_id", id},
+                    {"lat", lat},
+                    {"lng", lng},
+                    {"price",price},
+                    {"bed", bed},
+                    {"bath", bath},
+                    {"sf", sf},
+                    {"type",type},
+                    {"img",img}
+                    };
+
+
+                collection.InsertOneAsync(document);
+
+            }
+
+        }
+        private string url(double lat, double lng, double size, int page) {
+            string request = "http://www.zillow.com/search/GetResults.htm"
+                + "?" + "spt=homes"
+                + "&" + "status=110011"
+                + "&" + "lt=111101"
+                + "&" + "ht=111111"
+                + "&" + "pr=,"
+                + "&" + "mp=,"
+                + "&" + "bd=0%2C"
+                + "&" + "ba=0%2C"
+                + "&" + "sf=,"
+                + "&" + "lot=,"
+                + "&" + "yr=,"
+                + "&" + "pho=0"
+                + "&" + "pets=0"
+                + "&" + "parking=0"
+                + "&" + "laundry=0"
+                + "&" + "pnd=0"
+                + "&" + "red=0"
+                + "&" + "zso=0"
+                + "&" + "days=any"
+                + "&" + "ds=all"
+                + "&" + "pmf=1"
+                + "&" + "pf=1"
+                + "&" + "zoom=18"
+                + "&" + "rect="
+                + ( ( lat-( size*0.5 ) )*1000000.0 ).ToString()
+                + "," + ( ( lng-( size*0.5 ) )*1000000 ).ToString()
+                + "," + ( ( lat+( size*0.5 ) )*1000000 ).ToString()
+                + "," + ( ( lng+( size*0.5 ) )*1000000 ).ToString()
+                + "&" + "p="
+                + page.ToString()
+                + "&" + "sort=days"
+                + "&" + "search=maplist"
+                + "&" + "disp=1"
+                + "&" + "listright=true"
+                + "&" + "isMapSearch=true"
+                + "&" + "zoom=18";
+
+            //Console.WriteLine(request);
+            return request;
+        }
+        void writeDB() {
+
+
+
+            //MongoDB.Bson.BsonDocument doc;
+            //doc  = BsonDocument.Parse(json);
+
+
+            //var collection0 = test.GetCollection<BsonDocument>("prop");
+            //var filter = new BsonDocument();
+            //var count = 0;
+
+            //                var address = document["lat"].AsDouble;
+            //                Console.WriteLine(address.ToString());
+
+            //// build some test data
+            //BsonArray dataFields = new BsonArray { new BsonDocument { 
+            //    { "ID" , ObjectId.GenerateNewId()}, { "NAME", "ID"}, {"TYPE", "Text"} } };
+            //BsonDocument nested = new BsonDocument {
+            //    { "name", "John Doe" },
+            //    { "fields", dataFields },
+            //    { "address", new BsonDocument {
+            //            { "street", "123 Main St." },
+            //            { "city", "Madison" },
+            //            { "state", "WI" },
+            //            { "zip", 53711}
+            //        }
+            //    }
+            //};
+            //// grab the address from the document,
+            //// subdocs as a BsonDocument
+            //var address = nested["address"].AsBsonDocument;
+            //Console.WriteLine(address["city"].AsString); 
+            //// or, jump straight to the value ...
+            //Console.WriteLine(nested["address"]["city"].AsString);
+            //// loop through the fields array
+            //var allFields = nested["fields"].AsBsonArray ;
+            //foreach (var fields in allFields)
+            //{
+            //    // grab a few of the fields:
+            //    Console.WriteLine("Name: {0}, Type: {1}", 
+            //        fields["NAME"].AsString, fields["TYPE"].AsString);
+            //}
+
+            //Console.WriteLine(datas.map.nearbyProperties[i]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][1]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][2]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][7][0]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][7][1]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][7][2]);
+            //Console.WriteLine(datas.map.nearbyProperties[i][7][3]);
+        }
+        void writeFile() {
+
+            // WriteAllLines creates a file, writes a collection of strings to the file, and then closes the file.
+            //System.IO.File.WriteAllLines(@"C:\Users\JMorrison\Desktop\apts-" + today + ".txt", data2);
+
+
+
+            // Example #4: Append new text to an existing file. 
+            // The using statement automatically closes the stream and calls  
+            // IDisposable.Dispose on the stream object. 
+            //using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName, true)) {
+            //    for (int i = 0; i < data2.Count; i++) {
+            //        file.WriteLine(data2[i]);
+            //    }
+            //}        
+        }
+        private void pause() {
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadKey(false);
+        }
+
+
+    }
+
+}
